f79e8bdc9d15041fbbc18101356ac356f6698a36
utc-uvira <contact@utcuvira.org>
2026-01-27 01:40:52 -0800

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,10 @@
+print("Hello")
+nom=input("enter name")
+gender=input("M/F")
+
+if gender=="F":
+    print("Mm", nom)
+elif gender =="M":
+    print("Mr", nom)
+else:
+    print("gender must be m/m")
